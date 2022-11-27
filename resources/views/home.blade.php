aa8a95efe74ecc74724559ea18985f73debf7977
Fidel Reyes <freyesllerena33@gmail.com>
2022-11-27 20:20:15 +0100

    add the first test blade

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximun-scaled=1.0, minimun-scale=1.0">
+    <title>Mon mini-blog</title>
+</head>
+<body>
+<h1>Nos articles</h1>
+<ul>
+    @foreach($articles as $article)
+        <li>{{$article['title']}}</li>
+    @endforeach
+</ul>
+</body>
+</html>
